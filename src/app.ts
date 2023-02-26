7eb37f55f3de0e33ff12d9f0a4d2ccec16252f1e
Christopher Kalla <christopherkalla@christohersmbp2.lan>
2023-02-26 15:27:51 -0700

    setting up the rest of the api logic

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,20 @@
 import express, { Express } from 'express';
 import { RottenCornServer } from './setupServer';
 import databaseConnection from './setupDatabase';
+import { config } from './config';
 
 class Application {
-    public initialize(): void {
-        databaseConnection();
-        const app: Express = express();
-        const server: RottenCornServer = new RottenCornServer(app);
-        server.start();
-    }
+  public initialize(): void {
+    this.loadConfig();
+    databaseConnection();
+    const app: Express = express();
+    const server: RottenCornServer = new RottenCornServer(app);
+    server.start();
+  }
+
+  private loadConfig(): void {
+    config.validateConfig();
+  }
 }
 
 const application: Application = new Application();
--- src/setupDatabase.ts
+++ src/setupDatabase.ts
@@ -1,16 +1,23 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
+import Logger from 'bunyan';
+import { config } from './config';
+
+const log: Logger = config.createLogger('setupDatabase');
 
 export default () => {
-    const connect = () => {
-        mongoose.connect('mongodb://localhost:27017/rotterncornapp-backend').then(() => {
-            console.log('Successfully connected to database.');
-        })
-        .catch((error) => {
-            console.log('Error connecting to database', error);
-            return process.exit(1);
-        });
-    };
-    connect();
+  const connect = () => {
+    mongoose.set('strictQuery', false);
+    mongoose
+      .connect(`${config.DATABASE_URL}`)
+      .then(() => {
+        log.info('Successfully connected to database.');
+      })
+      .catch((error) => {
+        log.error('Error connecting to database', error);
+        return process.exit(1);
+      });
+  };
+  connect();
 
-    mongoose.connection.on('disconnected', connect);
+  mongoose.connection.on('disconnected', connect);
 };
