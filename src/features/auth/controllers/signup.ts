49adeea79c297c6f695be724fa3dcba9b8c242cd
Christopher Kalla <christopherkalla@christohersmbp2.lan>
2023-03-03 22:38:48 -0700

    adding files

---
+++ src/features/auth/controllers/signup.ts
@@ -0,0 +1,54 @@
+import HTTP_STATUS from 'http-status-codes';
+import { UploadApiResponse } from 'cloudinary';
+import { BadRequestError } from './../../../shared/globals/helpers/error-handler';
+import { authService } from './../../../shared/services/db/auth.service';
+import { IAuthDocument, ISignUpData } from './../interfaces/auth.interface';
+import { ObjectId } from 'mongodb';
+import { Request, Response } from 'express';
+import { joiValidation } from '@root/shared/decorators/joi-validation.decorators';
+import { signupSchema } from '@auth/schemes/signup';
+import { Helpers } from '@global/helpers/helpers';
+import { uploads } from '@global/helpers/cloudinary-upload';
+
+export class SignUp {
+  @joiValidation(signupSchema)
+  public async create(req: Request, res: Response): Promise<void> {
+    const { username, email, password, avatarColor, avatarImage } = req.body;
+    const checkIfUserExists: IAuthDocument = await authService.getUserByUsernameOrEmail(username, email);
+    if (checkIfUserExists) {
+      throw new BadRequestError('Invalid credentials');
+    }
+
+    const authObjectId: ObjectId = new ObjectId();
+    const userObjectId: ObjectId = new ObjectId();
+    const uId = `${Helpers.generateRandomIntegers(12)}`;
+    const authData: IAuthDocument = SignUp.prototype.signupData({
+      _id: authObjectId,
+      uId,
+      username,
+      email,
+      password,
+      avatarColor
+    });
+    console.info(avatarImage, "batman")
+    const result: UploadApiResponse = await uploads(avatarImage, `${userObjectId}`, true, true) as UploadApiResponse;
+    if (!result?.public_id) {
+      throw new BadRequestError('File upload: Eerror occured. Try again.');
+    }
+
+    res.status(HTTP_STATUS.CREATED).json({ message: 'User created successfully', authData});
+  }
+
+  private signupData(data: ISignUpData): IAuthDocument {
+    const { _id, username, email, uId, password, avatarColor } = data;
+    return {
+      _id,
+      uId,
+      username: Helpers.firstLetterUppercase(username),
+      email: Helpers.lowerCase(email),
+      password,
+      avatarColor,
+      createdAt: new Date()
+    } as IAuthDocument;
+  }
+}
--- src/shared/globals/helpers/cloudinary-upload.ts
+++ src/shared/globals/helpers/cloudinary-upload.ts
@@ -1,22 +1,24 @@
-import cloudinary, { UploadApiResponse, UploadApiErrorResponse} from 'cloudinary'
+import cloudinary, { UploadApiResponse, UploadApiErrorResponse} from 'cloudinary';
 
 export function uploads(
   file: string,
   public_id?: string,
   overwrite?: boolean,
-  invalidate?: boolean
+  invalidate?: boolean,
+  resource_type?: string,
 ): Promise<UploadApiResponse | UploadApiErrorResponse | undefined> {
   return new Promise((resolve) => {
+    console.info({file, public_id, overwrite,invalidate, resource_type});
     cloudinary.v2.uploader.upload(
       file,
       {
         public_id,
         overwrite,
-        invalidate
+        invalidate,
       },
       (error: UploadApiErrorResponse | undefined, result: UploadApiResponse | undefined) => {
-        if (error) resolve(error);
-        resolve(result);
+         if (error) resolve(error);
+         resolve(result);
       }
     );
   });
