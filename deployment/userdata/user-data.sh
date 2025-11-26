6edb8989f0770b4009e915e479b100f0938a6937
Christopher Kalla <christopherkalla@macbookpro.lan>
2025-11-26 12:30:58 -0700

    fixing staging branch

--- deployment/userdata/user-data.sh
+++ deployment/userdata/user-data.sh
@@ -37,4 +37,12 @@ if [ $(program_is_installed pm2) == 0 ]; then
 fi
 
 cd /home/ec2-user
+git clone -b staging https://github.com/uzochukwueddie/chatty-backend.git
+cd chatty-backend
+npm install
+aws s3 sync s3://chattapplication1-env-files/staging .
+unzip env-file.zip
+cp .env.staging .env
+npm run build
+npm run start
 
